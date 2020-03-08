b0612fe3ea7e37e31b0d0d48c570274d8a4127e2
Robbie Gill <34260042+robbieGill4096@users.noreply.github.com>
2020-03-08 14:04:58 -0600

    Update smash.c

--- smash.c
+++ smash.c
@@ -1,16 +1,28 @@
+//	**library defined**
+//---------------------------
+//---------------------------
 #include<stdio.h>
 #include<string.h>
 #include <errno.h>
 #include <unistd.h>
 #include<stdlib.h>
+
+//	**User Define**
+//---------------------------
+//---------------------------
+#include "history.h"
+//	**Constants**
+//---------------------------
+//---------------------------
 #define MAXLINE 4096
-//**Method Prototypes**
+#define PATHLEN 100 // length of the current path to return to the user.
+
+//	**Method Prototypes**
 //---------------------------
 //---------------------------
-void executeCommand(char*);
-int numberOfCommands(char*);
-int command_selection(char*);
-char*getcwd(char*,size_t);
+void executeCommand(char*);//executes a user command from keyboard input.
+int numberOfCommands(char*);//returns the (int) number of commands to the user tokenized by " "
+char*getcwd(char*,size_t);// returns the current working directory as a string.
 //---------------------------
 char bfr[MAXLINE];
 //
@@ -18,91 +30,93 @@ char bfr[MAXLINE];
 //	**Looping prompt for the Smash SHELL** 
 //--------------------------------------------------------------
 //--------------------------------------------------------------
-int main(){
-
+int main()
+{
 // printing current working directory  
 fputs("$",stderr); //output the first prompt
-
 //loop until EOF or error
-	while (fgets(bfr, MAXLINE,stdin) != NULL) {
-
-		if (strlen(bfr) == MAXLINE-1){//checks for overflow. 
+	while (fgets(bfr, MAXLINE,stdin) != NULL) 
+	{
+		if (strlen(bfr) == MAXLINE-1)//checks for overflow from user input.
+		{ 
 			return -1;
-			}
-		else{
-			
+		}
+		else
+		{
 			bfr[strlen(bfr)-1] = '\0'; //replace newline with null
 			executeCommand(bfr);//puts(bfr);
-			
 			fputs("$",stderr);
-			}}
-		return 0;
 		}
-
+	}
+		return 0;
+}
 //	**parse its single parameter into space-seperated tokens using the strtok**
 //--------------------------------------------------------------
 //--------------------------------------------------------------
-void executeCommand(char *str){
-	char s[100]; 
+void executeCommand(char *str)
+{
+	char current_dir_path[PATHLEN]; 
 	int command_count = numberOfCommands(bfr);
 	char *args[command_count];
 	int i =0;
 	char *p = strtok (str," ");
 	while (p != NULL)
-		{
+	{
 		args[i++] = p;
-		p = strtok (NULL, " "); }
+		p = strtok (NULL, " "); 
+	}
 //--------------------------------------------------------------
 //--------------------------------------------------------------
-
 	if (strcmp(args[0], "exit") == 0) 
 	{
 	  exit(0);
 	} 
+//	**CD COMMAND**
 //---------------------------------------
 //---------------------------------------
 	else if (strcmp(args[0], "cd") == 0)
 	{
-	if(command_count >=2){
-		if(chdir(args[1]) == 0){
-		printf("%s\n",getcwd(s, 100)); }
-		else{
-		printf("%s:No such file or directory\n",args[1]);
+		if(command_count >=2) // makes sure cd also contains a path name.
+		{
+			if(chdir(args[1]) == 0)//checks that directory user attempted to cd to exists 0 ==true
+			{
+				printf("%s\n",getcwd(current_dir_path, PATHLEN)); 
+			}
+			else //if directory is not found prints error message including failed directory name. 
+			{
+			printf("%s:No such file or directory\n",args[1]);
+			}
 		}
-		
 	}
-}
 //---------------------------------------
 //---------------------------------------
 	else if (strcmp(args[0], "history") == 0)
 	{
-
+		//print_history();
 	}
-
-	/* default: */
-	else {
-		//printf("%s: command not found\n",args[0]);
-		for (int index=0;index < command_count;index++){
-		printf("args[%d] %s\n",index,args[index]);		
-	}		
-
-
+	else //command is unrecognized  
+	{
+		for (int index=0;index < command_count;index++)
+		{
+			printf("args[%d] %s\n",index,args[index]);		
+		}		
 	}
 }
 //	**calculates the number of commands read in from a pointer**
 //--------------------------------------------------------------
 //--------------------------------------------------------------
-int numberOfCommands(char *str1) {
+int numberOfCommands(char *str1) 
+{
 	char copy[strlen(str1)];
 	strncpy(copy,str1,strlen(str1));
 	int command_count = 0;
 	char delim[] = " ";
 	char* token1;
 
-
-	for (token1 = strtok(copy, delim); token1; token1 = strtok(NULL, delim)) {
-   	 command_count+=1;
-			}
-
-		return command_count;}
+	for (token1 = strtok(copy, delim); token1; token1 = strtok(NULL, delim)) 
+	{
+   		command_count+=1;
+	}
+	return command_count;
+}
 
