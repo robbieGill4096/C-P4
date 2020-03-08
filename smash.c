9ab1d09a5686fff2e81bc990af76608468ca1faa
Robbie Gill <34260042+robbieGill4096@users.noreply.github.com>
2020-03-08 13:36:15 -0600

    Update smash.c

--- smash.c
+++ smash.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<string.h>
+#include <errno.h>
+#include <unistd.h>
 #include<stdlib.h>
 #define MAXLINE 4096
 //**Method Prototypes**
@@ -8,13 +10,17 @@
 void executeCommand(char*);
 int numberOfCommands(char*);
 int command_selection(char*);
+char*getcwd(char*,size_t);
 //---------------------------
 char bfr[MAXLINE];
-
+//
+//printf(" %s:%d\n ",args[1],errno); general error message.
 //	**Looping prompt for the Smash SHELL** 
 //--------------------------------------------------------------
 //--------------------------------------------------------------
 int main(){
+
+// printing current working directory  
 fputs("$",stderr); //output the first prompt
 
 //loop until EOF or error
@@ -27,7 +33,8 @@ fputs("$",stderr); //output the first prompt
 			
 			bfr[strlen(bfr)-1] = '\0'; //replace newline with null
 			executeCommand(bfr);//puts(bfr);
-			fputs("$ ",stderr);
+			
+			fputs("$",stderr);
 			}}
 		return 0;
 		}
@@ -36,6 +43,7 @@ fputs("$",stderr); //output the first prompt
 //--------------------------------------------------------------
 //--------------------------------------------------------------
 void executeCommand(char *str){
+	char s[100]; 
 	int command_count = numberOfCommands(bfr);
 	char *args[command_count];
 	int i =0;
@@ -44,12 +52,42 @@ void executeCommand(char *str){
 		{
 		args[i++] = p;
 		p = strtok (NULL, " "); }
+//--------------------------------------------------------------
+//--------------------------------------------------------------
 
-		for (i = 0; i< command_count; ++i)
-		{	printf("%s\n",args[i]);
-		
+	if (strcmp(args[0], "exit") == 0) 
+	{
+	  exit(0);
+	} 
+//---------------------------------------
+//---------------------------------------
+	else if (strcmp(args[0], "cd") == 0)
+	{
+	if(command_count >=2){
+		if(chdir(args[1]) == 0){
+		printf("%s\n",getcwd(s, 100)); }
+		else{
+		printf("%s:No such file or directory\n",args[1]);
 		}
-	command_selection(args[0]);
+		
+	}
+}
+//---------------------------------------
+//---------------------------------------
+	else if (strcmp(args[0], "history") == 0)
+	{
+
+	}
+
+	/* default: */
+	else {
+		//printf("%s: command not found\n",args[0]);
+		for (int index=0;index < command_count;index++){
+		printf("args[%d] %s\n",index,args[index]);		
+	}		
+
+
+	}
 }
 //	**calculates the number of commands read in from a pointer**
 //--------------------------------------------------------------
@@ -67,48 +105,4 @@ int numberOfCommands(char *str1) {
 			}
 
 		return command_count;}
-//	**Makes a selection based on a Command**
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-int command_selection(char *command){
-//---------------------------------------
-//---------------------------------------
-	if (strcmp(command, "exit") == 0) 
-	{
-printf("choice of exit is made");
-	  exit(0);
-	} 
-//---------------------------------------
-//---------------------------------------
-	else if (strcmp(command, "cd") == 0)
-	{
 
-	}
-//---------------------------------------
-//---------------------------------------
-	else if (strcmp(command, "") == 0)
-	{
-
-	}
-//---------------------------------------
-//---------------------------------------
-	else if (strcmp(command, "") == 0)
-	{
-
-	}
-//---------------------------------------
-//---------------------------------------
-	else if (strcmp(command, "") == 0)
-	{
-
-	}
-	/* more else if clauses */
-	else /* default: */
-{
-}
-
-
-
-
-
-}
